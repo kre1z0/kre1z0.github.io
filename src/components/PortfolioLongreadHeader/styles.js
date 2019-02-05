2858056741b1f0e6d5496d298a200b3595335366
kreizo <thekreizo@gmail.com>
2019-02-05 15:45:17 +0200

    longread fix

--- src/components/PortfolioLongreadHeader/styles.js
+++ src/components/PortfolioLongreadHeader/styles.js
@@ -81,11 +81,8 @@ export const RightSide = styled(Section)`
     flex-shrink: 0;
     flex-grow: 1;
   }
-  @media (max-width: 812px) and (max-height: 275px) and (orientation: landscape), only screen 
-  and (min-device-width: 320px) 
-  and (max-device-width: 568px)
-  and (-webkit-min-device-pixel-ratio: 2)
-  and (orientation: landscape) {
+  @media (max-width: 812px) and (max-height: 320px) and (orientation: landscape),
+    only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
     margin-top: 2rem;
     width: 34%;
   }
@@ -118,11 +115,8 @@ export const Badge = styled("label")`
     padding: 0 0.44rem;
     font-size: 0.7142rem;
   }
-  @media (max-width: 812px) and (max-height: 275px) and (orientation: landscape), only screen 
-  and (min-device-width: 320px) 
-  and (max-device-width: 568px)
-  and (-webkit-min-device-pixel-ratio: 2)
-  and (orientation: landscape) {
+  @media (max-width: 812px) and (max-height: 275px) and (orientation: landscape),
+    only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
     display: none;
   }
 `;
