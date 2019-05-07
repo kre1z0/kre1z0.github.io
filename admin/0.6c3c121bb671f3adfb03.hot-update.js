ed0c9102689c99d414eda99f9ae36bc9d2c920be
Igor <thekreizo@gmail.com>
2019-05-07 14:13:26 +0300

    Updates

---
+++ admin/0.6c3c121bb671f3adfb03.hot-update.js
@@ -0,0 +1,208 @@
+webpackHotUpdate(0,{
+
+/***/ 650:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var _img_development_ycle_1_png__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(651);
+/* harmony import */ var _img_development_ycle_1_png__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_1_png__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _img_development_ycle_1_2x_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(652);
+/* harmony import */ var _img_development_ycle_1_2x_png__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_1_2x_png__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _img_development_ycle_1_3x_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(653);
+/* harmony import */ var _img_development_ycle_1_3x_png__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_1_3x_png__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _assets_img_development_ycle_dividers_separate_1_svg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(654);
+/* harmony import */ var _img_development_ycle_2_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(655);
+/* harmony import */ var _img_development_ycle_2_png__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_2_png__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var _img_development_ycle_2_2x_png__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(656);
+/* harmony import */ var _img_development_ycle_2_2x_png__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_2_2x_png__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var _img_development_ycle_2_3x_png__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(657);
+/* harmony import */ var _img_development_ycle_2_3x_png__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_2_3x_png__WEBPACK_IMPORTED_MODULE_6__);
+/* harmony import */ var _assets_img_development_ycle_dividers_separate_2_svg__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(658);
+/* harmony import */ var _img_development_ycle_3_3_png__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(659);
+/* harmony import */ var _img_development_ycle_3_3_png__WEBPACK_IMPORTED_MODULE_8___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_3_3_png__WEBPACK_IMPORTED_MODULE_8__);
+/* harmony import */ var _img_development_ycle_3_3_2x_png__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(660);
+/* harmony import */ var _img_development_ycle_3_3_2x_png__WEBPACK_IMPORTED_MODULE_9___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_3_3_2x_png__WEBPACK_IMPORTED_MODULE_9__);
+/* harmony import */ var _img_development_ycle_3_3_3x_png__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(661);
+/* harmony import */ var _img_development_ycle_3_3_3x_png__WEBPACK_IMPORTED_MODULE_10___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_3_3_3x_png__WEBPACK_IMPORTED_MODULE_10__);
+/* harmony import */ var _assets_img_development_ycle_dividers_separate_3_svg__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(662);
+/* harmony import */ var _img_development_ycle_4_2_x_png__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(663);
+/* harmony import */ var _img_development_ycle_4_2_x_png__WEBPACK_IMPORTED_MODULE_12___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_4_2_x_png__WEBPACK_IMPORTED_MODULE_12__);
+/* harmony import */ var _img_development_ycle_4_2_x_2x_png__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(664);
+/* harmony import */ var _img_development_ycle_4_2_x_2x_png__WEBPACK_IMPORTED_MODULE_13___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_4_2_x_2x_png__WEBPACK_IMPORTED_MODULE_13__);
+/* harmony import */ var _img_development_ycle_4_2_x_3x_png__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(665);
+/* harmony import */ var _img_development_ycle_4_2_x_3x_png__WEBPACK_IMPORTED_MODULE_14___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_4_2_x_3x_png__WEBPACK_IMPORTED_MODULE_14__);
+/* harmony import */ var _assets_img_development_ycle_dividers_separate_4_svg__WEBPACK_IMPORTED_MODULE_15__ = __webpack_require__(666);
+/* harmony import */ var _img_development_ycle_5_png__WEBPACK_IMPORTED_MODULE_16__ = __webpack_require__(667);
+/* harmony import */ var _img_development_ycle_5_png__WEBPACK_IMPORTED_MODULE_16___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_5_png__WEBPACK_IMPORTED_MODULE_16__);
+/* harmony import */ var _img_development_ycle_5_2x_png__WEBPACK_IMPORTED_MODULE_17__ = __webpack_require__(668);
+/* harmony import */ var _img_development_ycle_5_2x_png__WEBPACK_IMPORTED_MODULE_17___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_5_2x_png__WEBPACK_IMPORTED_MODULE_17__);
+/* harmony import */ var _img_development_ycle_5_3x_png__WEBPACK_IMPORTED_MODULE_18__ = __webpack_require__(669);
+/* harmony import */ var _img_development_ycle_5_3x_png__WEBPACK_IMPORTED_MODULE_18___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_5_3x_png__WEBPACK_IMPORTED_MODULE_18__);
+/* harmony import */ var _assets_img_development_ycle_dividers_separate_5_svg__WEBPACK_IMPORTED_MODULE_19__ = __webpack_require__(670);
+/* harmony import */ var _img_development_ycle_6_png__WEBPACK_IMPORTED_MODULE_20__ = __webpack_require__(671);
+/* harmony import */ var _img_development_ycle_6_png__WEBPACK_IMPORTED_MODULE_20___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_6_png__WEBPACK_IMPORTED_MODULE_20__);
+/* harmony import */ var _img_development_ycle_6_2x_png__WEBPACK_IMPORTED_MODULE_21__ = __webpack_require__(672);
+/* harmony import */ var _img_development_ycle_6_2x_png__WEBPACK_IMPORTED_MODULE_21___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_6_2x_png__WEBPACK_IMPORTED_MODULE_21__);
+/* harmony import */ var _img_development_ycle_6_3x_png__WEBPACK_IMPORTED_MODULE_22__ = __webpack_require__(673);
+/* harmony import */ var _img_development_ycle_6_3x_png__WEBPACK_IMPORTED_MODULE_22___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_6_3x_png__WEBPACK_IMPORTED_MODULE_22__);
+/* harmony import */ var _assets_img_development_ycle_dividers_separate_6_svg__WEBPACK_IMPORTED_MODULE_23__ = __webpack_require__(674);
+/* harmony import */ var _img_development_ycle_7_2_png__WEBPACK_IMPORTED_MODULE_24__ = __webpack_require__(675);
+/* harmony import */ var _img_development_ycle_7_2_png__WEBPACK_IMPORTED_MODULE_24___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_7_2_png__WEBPACK_IMPORTED_MODULE_24__);
+/* harmony import */ var _img_development_ycle_7_2_2x_png__WEBPACK_IMPORTED_MODULE_25__ = __webpack_require__(676);
+/* harmony import */ var _img_development_ycle_7_2_2x_png__WEBPACK_IMPORTED_MODULE_25___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_7_2_2x_png__WEBPACK_IMPORTED_MODULE_25__);
+/* harmony import */ var _img_development_ycle_7_2_3x_png__WEBPACK_IMPORTED_MODULE_26__ = __webpack_require__(677);
+/* harmony import */ var _img_development_ycle_7_2_3x_png__WEBPACK_IMPORTED_MODULE_26___default = /*#__PURE__*/__webpack_require__.n(_img_development_ycle_7_2_3x_png__WEBPACK_IMPORTED_MODULE_26__);
+(function () {
+  var enterModule = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(24)).enterModule;
+  enterModule && enterModule(module);
+})();
+
+(function () {
+  var enterModule = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(24)).enterModule;
+  enterModule && enterModule(module);
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var _default = {
+  title: "Цикл разработки",
+  blocks: [{
+    name: "Понимание проблемы",
+    description: "Хороший проект всегда начинается с выявления потребностей и правильной постановки целей. Это работа менеджеров и аналитиков. Остальных членов команды мы тоже держим в курсе и привлекаем, когда требуется их участие. Взаимодействие всех участников на разных этапах разработки позволяет создать лучшее решение.",
+    img: {
+      x1: _img_development_ycle_1_png__WEBPACK_IMPORTED_MODULE_0___default.a,
+      x2: _img_development_ycle_1_2x_png__WEBPACK_IMPORTED_MODULE_1___default.a,
+      x3: _img_development_ycle_1_3x_png__WEBPACK_IMPORTED_MODULE_2___default.a
+    },
+    Element: _assets_img_development_ycle_dividers_separate_1_svg__WEBPACK_IMPORTED_MODULE_3__["ReactComponent"]
+  }, {
+    name: "Формализация и планирование",
+    description: "Второй этап — формализация требований, расстановка приоритетов и планирование задач. Мы используем метод Scrum, но адаптировали его под себя. Мы работаем в рамках спринтов, а вместо ежедневных стендапов рассказываем в Slack о текущем состоянии дел. Раз в неделю проводим проектные статус-митинги.",
+    img: {
+      x1: _img_development_ycle_2_png__WEBPACK_IMPORTED_MODULE_4___default.a,
+      x2: _img_development_ycle_2_2x_png__WEBPACK_IMPORTED_MODULE_5___default.a,
+      x3: _img_development_ycle_2_3x_png__WEBPACK_IMPORTED_MODULE_6___default.a
+    },
+    Element: _assets_img_development_ycle_dividers_separate_2_svg__WEBPACK_IMPORTED_MODULE_7__["ReactComponent"]
+  }, {
+    name: "Проектирование и дизайн",
+    description: "Задача наших UX/UI-дизайнеров учесть все бизнес-требования, тренды веб-дизайна, технические ограничения и спроектировать красивый и удобный интерфейс. Для максимально эффективного дизайна и разработки мы создали и развиваем свою дизайн-систему, ориентированную на решение геоинформационных задач.",
+    img: {
+      x1: _img_development_ycle_3_3_png__WEBPACK_IMPORTED_MODULE_8___default.a,
+      x2: _img_development_ycle_3_3_2x_png__WEBPACK_IMPORTED_MODULE_9___default.a,
+      x3: _img_development_ycle_3_3_3x_png__WEBPACK_IMPORTED_MODULE_10___default.a
+    },
+    Element: _assets_img_development_ycle_dividers_separate_3_svg__WEBPACK_IMPORTED_MODULE_11__["ReactComponent"],
+    offset: 12
+  }, {
+    name: "Архитектурное проектирование",
+    description: "Да, мы действительно много времени уделяем планированию, подготовке требований, интерфейсному и техническому проектированию — это 70% успеха. Архитектор совместно с разработчиками продумывает, как будет работать продукт, какие технологии использовать, чтобы реализовать требования и идеи менеджеров, аналитиков, дизайнеров качественно и в срок.",
+    img: {
+      x1: _img_development_ycle_4_2_x_png__WEBPACK_IMPORTED_MODULE_12___default.a,
+      x2: _img_development_ycle_4_2_x_2x_png__WEBPACK_IMPORTED_MODULE_13___default.a,
+      x3: _img_development_ycle_4_2_x_3x_png__WEBPACK_IMPORTED_MODULE_14___default.a
+    },
+    Element: _assets_img_development_ycle_dividers_separate_4_svg__WEBPACK_IMPORTED_MODULE_15__["ReactComponent"],
+    offset: 26
+  }, {
+    name: "Разработка",
+    description: ["В разработку попадают максимально продуманные задачи. Однако всем известно, что предусмотреть все невозможно, поэтому мы и любим Scrum: гибкость позволяет нам быстро реагировать на изменение требований.", "Если в ходе работы кто-то проявляет инициативу взять на себя задачу посложнее и выйти на новый уровень компетенций - мы поддержим и поможем. Мы уже вырастили несколько специалистов внутри компании. Например, наш главный архитектор вырос из junior-разработчика."],
+    img: {
+      x1: _img_development_ycle_5_png__WEBPACK_IMPORTED_MODULE_16___default.a,
+      x2: _img_development_ycle_5_2x_png__WEBPACK_IMPORTED_MODULE_17___default.a,
+      x3: _img_development_ycle_5_3x_png__WEBPACK_IMPORTED_MODULE_18___default.a
+    },
+    Element: _assets_img_development_ycle_dividers_separate_5_svg__WEBPACK_IMPORTED_MODULE_19__["ReactComponent"],
+    offset: 16
+  }, {
+    name: "Тестирование",
+    description: ["Проверка проходит в несколько этапов: сначала разработчик проверяет задачу сам, потом отдает на ревью другому разработчику.", "Функциональность тестирует тот, кто поставил задачу: обычно это аналитик или менеджер. Иногда к тестированию подключают других членов команды: ГИС-специалистов, специалистов по внедрению."],
+    img: {
+      x1: _img_development_ycle_6_png__WEBPACK_IMPORTED_MODULE_20___default.a,
+      x2: _img_development_ycle_6_2x_png__WEBPACK_IMPORTED_MODULE_21___default.a,
+      x3: _img_development_ycle_6_3x_png__WEBPACK_IMPORTED_MODULE_22___default.a
+    },
+    Element: _assets_img_development_ycle_dividers_separate_6_svg__WEBPACK_IMPORTED_MODULE_23__["ReactComponent"],
+    offset: 26
+  }, {
+    name: "Релиз",
+    description: "Все вместе радуемся выходу продукта, новой функции и ещё одному довольному заказчику!",
+    img: {
+      x1: _img_development_ycle_7_2_png__WEBPACK_IMPORTED_MODULE_24___default.a,
+      x2: _img_development_ycle_7_2_2x_png__WEBPACK_IMPORTED_MODULE_25___default.a,
+      x3: _img_development_ycle_7_2_3x_png__WEBPACK_IMPORTED_MODULE_26___default.a
+    }
+  }]
+};
+var _default2 = _default;
+/* harmony default export */ __webpack_exports__["default"] = (_default2);
+;
+
+(function () {
+  var reactHotLoader = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(24)).default;
+
+  if (!reactHotLoader) {
+    return;
+  }
+
+  reactHotLoader.register(_default, "default", "/home/kre1z0/Projects/everpoint-site/src/assets/developmentCycle.js");
+})();
+
+;
+
+(function () {
+  var leaveModule = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(24)).leaveModule;
+  leaveModule && leaveModule(module);
+})();
+
+;
+
+(function () {
+  var reactHotLoader = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(24)).default;
+
+  if (!reactHotLoader) {
+    return;
+  }
+
+  reactHotLoader.register(_default, "_default", "/home/kre1z0/Projects/everpoint-site/src/assets/developmentCycle.js");
+  reactHotLoader.register(_default2, "default", "/home/kre1z0/Projects/everpoint-site/src/assets/developmentCycle.js");
+})();
+
+;
+
+(function () {
+  var leaveModule = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(24)).leaveModule;
+  leaveModule && leaveModule(module);
+})();
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(23)(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=0.6c3c121bb671f3adfb03.hot-update.js.map
---
+++ admin/0.ddf58422a2177af7a612.hot-update.js
@@ -0,0 +1,127 @@
+webpackHotUpdate(0,{
+
+/***/ 928:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "HowWeAreWorking", function() { return HowWeAreWorking; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(8);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(38);
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_GoNextLink_GoNextLink__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(548);
+/* harmony import */ var _Principle_Principle__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(929);
+/* harmony import */ var _styles__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(937);
+(function () {
+  var enterModule = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(24)).enterModule;
+  enterModule && enterModule(module);
+})();
+
+var _jsxFileName = "/home/kre1z0/Projects/everpoint-site/src/components/HowWeAreWorking/HowWeAreWorking.js";
+
+(function () {
+  var enterModule = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(24)).enterModule;
+  enterModule && enterModule(module);
+})();
+
+
+
+
+
+
+var HowWeAreWorking = function HowWeAreWorking(_ref) {
+  var items = _ref.items,
+      className = _ref.className;
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles__WEBPACK_IMPORTED_MODULE_4__["HowWeAreWorkingContainer"], {
+    className: className,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles__WEBPACK_IMPORTED_MODULE_4__["Title"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, "\u0420\u0430\u0431\u043E\u0442\u0430 \u0432 Everpoint"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles__WEBPACK_IMPORTED_MODULE_4__["Paragraph"], {
+    className: _styles__WEBPACK_IMPORTED_MODULE_4__["default"].howWeAreWorkingParagraph,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, "\u0411\u043E\u043B\u0435\u0435 15 \u043B\u0435\u0442 \u043C\u044B \u0440\u0430\u0437\u0440\u0430\u0431\u0430\u0442\u044B\u0432\u0430\u0435\u043C \u0433\u0435\u043E\u0441\u0435\u0440\u0432\u0438\u0441\u044B \u0434\u043B\u044F \u043D\u0430\u0448\u0438\u0445 \u0437\u0430\u043A\u0430\u0437\u0447\u0438\u043A\u043E\u0432, \u0430 \u0441 2018 \u0433\u043E\u0434\u0430 \u0440\u0430\u0437\u0432\u0438\u0432\u0430\u0435\u043C \u0438 \u0441\u043E\u0431\u0441\u0442\u0432\u0435\u043D\u043D\u0443\u044E \u0433\u0435\u043E\u0438\u043D\u0444\u043E\u0440\u043C\u0430\u0446\u0438\u043E\u043D\u043D\u0443\u044E \u0441\u0438\u0441\u0442\u0435\u043C\u0443 EverGIS. \u0420\u0430\u0431\u043E\u0442\u0430 \u0443 \u043D\u0430\u0441 \u2014 \u044D\u0442\u043E \u0432\u0441\u0435\u0433\u0434\u0430 \u0438\u043D\u0442\u0435\u0440\u0435\u0441\u043D\u044B\u0435 \u0437\u0430\u0434\u0430\u0447\u0438, \u0432\u043E\u0437\u043C\u043E\u0436\u043D\u043E\u0441\u0442\u044C \u043F\u0440\u043E\u044F\u0432\u0438\u0442\u044C \u0441\u0435\u0431\u044F \u0438 \u043F\u043E\u0434\u0434\u0435\u0440\u0436\u043A\u0430 \u043A\u043E\u043B\u043B\u0435\u043A\u0442\u0438\u0432\u0430. \u0411\u0443\u0434\u0435\u043C \u0440\u0430\u0434\u044B \u0432\u0438\u0434\u0435\u0442\u044C \u0443 \u0441\u0435\u0431\u044F \u0432 \u043A\u043E\u043C\u0430\u043D\u0434\u0435 \u0442\u0430\u043A\u0438\u0445 \u0436\u0435 \u0446\u0435\u043B\u0435\u0443\u0441\u0442\u0440\u0435\u043C\u043B\u0435\u043D\u043D\u044B\u0445, \u0430\u043A\u0442\u0438\u0432\u043D\u044B\u0445 \u0438 \u043B\u044E\u0431\u043E\u0437\u043D\u0430\u0442\u0435\u043B\u044C\u043D\u044B\u0445 \u043B\u044E\u0434\u0435\u0439!"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles__WEBPACK_IMPORTED_MODULE_4__["Title"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18
+    },
+    __self: this
+  }, "\u0414\u043B\u044F \u043D\u0430\u0441 \u0432\u0430\u0436\u043D\u044B"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Principle_Principle__WEBPACK_IMPORTED_MODULE_3__["Principle"], {
+    items: items,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_GoNextLink_GoNextLink__WEBPACK_IMPORTED_MODULE_2__["GoNextLink"], {
+    to: "/work",
+    gatsby: true,
+    className: _styles__WEBPACK_IMPORTED_MODULE_4__["default"].howWeAreWorkingLink,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20
+    },
+    __self: this
+  }, "\u041F\u043E\u0434\u0440\u043E\u0431\u043D\u0435\u0435 \u043E \u0440\u0430\u0431\u043E\u0442\u0435 \u0432 Everpoint"));
+};
+HowWeAreWorking.propTypes = {
+  items: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.arrayOf(prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.object),
+  className: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.string
+};
+;
+
+(function () {
+  var reactHotLoader = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(24)).default;
+
+  if (!reactHotLoader) {
+    return;
+  }
+
+  reactHotLoader.register(HowWeAreWorking, "HowWeAreWorking", "/home/kre1z0/Projects/everpoint-site/src/components/HowWeAreWorking/HowWeAreWorking.js");
+})();
+
+;
+
+(function () {
+  var leaveModule = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(24)).leaveModule;
+  leaveModule && leaveModule(module);
+})();
+
+;
+
+(function () {
+  var reactHotLoader = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(24)).default;
+
+  if (!reactHotLoader) {
+    return;
+  }
+
+  reactHotLoader.register(_jsxFileName, "_jsxFileName", "/home/kre1z0/Projects/everpoint-site/src/components/HowWeAreWorking/HowWeAreWorking.js");
+  reactHotLoader.register(HowWeAreWorking, "HowWeAreWorking", "/home/kre1z0/Projects/everpoint-site/src/components/HowWeAreWorking/HowWeAreWorking.js");
+})();
+
+;
+
+(function () {
+  var leaveModule = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(24)).leaveModule;
+  leaveModule && leaveModule(module);
+})();
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(23)(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=0.ddf58422a2177af7a612.hot-update.js.map
