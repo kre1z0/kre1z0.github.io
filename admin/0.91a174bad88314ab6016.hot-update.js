b79cd86ba2b877146f97703bb0ea8a5d3e7aba01
Igor <thekreizo@gmail.com>
2019-05-13 16:47:26 +0300

    Updates

---
+++ admin/0.91a174bad88314ab6016.hot-update.js
@@ -0,0 +1,25 @@
+webpackHotUpdate(0,{
+
+/***/ 620:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/bank-66c2a2163d71cfb9339d9cc2c43276c7.png";
+
+/***/ }),
+
+/***/ 621:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/bank@2x-f6475f8ce398e73c4c72e3d9ce0f2f27.png";
+
+/***/ }),
+
+/***/ 622:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/bank@3x-59a95e78f4e2832090f4dd61979cf548.png";
+
+/***/ })
+
+})
+//# sourceMappingURL=0.91a174bad88314ab6016.hot-update.js.map
